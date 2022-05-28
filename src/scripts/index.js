1c7dbd29b5c2882a4ec6636a3dd78f0ad315ba79
semen.tishchenko <semen.tishchenko@glowbyteconsulting.com>
2022-05-28 18:27:21 +0300

    feat: правка общего layout

--- src/scripts/index.js
+++
@@ -1,8 +0,0 @@
-// const isAuth = true;
-
-// if (isAuth) {
-//     history.replaceState({}, '', '/login.hbs')
-// } else {
-//     history.replaceState({}, '', '/app.hbs')
-// }
-// console.log('hellop from index.js')
