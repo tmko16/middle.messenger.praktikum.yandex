03568f0c0523990f0826692e13ab3bf71fcbb8e2
tmko16 <simontis1991@gmail.com>
2022-07-13 23:15:23 +0300

    refactor: заменил a тэг на компонент Link

--- src/pages/404/page404.ts
+++ src/pages/404/page404.ts
@@ -1,14 +1,25 @@
 import Block from "../../core/Block";
 import './404.less'
+import Link from "../../components/link";
+
 export class Page404 extends Block {
+    constructor() {
+        super();
+        this.setChildren({
+            backLink: new Link({text: "Назад к чатам", to: "index"})
+        })
+    }
+
     protected render(): string {
         //language=hbs
         return `
             <div class="page-404">
                 <div class="page-404__content">
-                    <span  class="page-404__title">404</span>
-                    <span  class="page-404__subtitle">Не туда попали</span>
-                    <a  class="page-404__msg" href="index.hbs"> <span>Назад к чатам</span></a>
+                    <span class="page-404__title">404</span>
+                    <span class="page-404__subtitle">Не туда попали</span>
+                    <div class="page-404__msg">
+                        {{backLink}}
+                    </div>
                 </div>
             </div>
         `
