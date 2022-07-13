d754519470833aa17d14a63b77ba953957837ab3
tmko16 <simontis1991@gmail.com>
2022-07-13 21:27:59 +0300

    feat:  валидация страница регистрации

--- src/pages/registration/registrationPage.ts
+++ src/pages/registration/registrationPage.ts
@@ -3,29 +3,46 @@ import './registrationPage.less';
 import FormInput from "../../components/formInput";
 import Button from "../../components/button";
 import {
-    emailValidation,
+    emailValidation, loginValidation,
     nameValidation,
     onSubmitValidation,
     passwordValidation,
     phoneValidation
 } from "../../utils/validators";
+import {getFormValues} from "../../utils/getFormValues";
 
 export class RegistrationPage extends Block {
+    protected formValues: any = {};
 
     constructor() {
         super({});
 
         this.setChildren({
             button: new Button({
-                text: "Регистрация", classes: "btn_l", href: "#", onSubmit:  onSubmitValidation.bind(this)
+                text: "Регистрация", classes: "btn_l", href: "#",  onSubmit: this.onSubmitHandler.bind(this)
             }),
             email: new FormInput({label: "Почта", name: "email", type: "text", validation: emailValidation}),
-            login: new FormInput({label: "Логин", name: "login", type: "text", validation: passwordValidation}),
+            login: new FormInput({label: "Логин", name: "login", type: "text", validation: loginValidation}),
             first_name: new FormInput({label: "Имя", name: "first_name", type: "text", validation: nameValidation}),
-            second_name: new FormInput({label: "Фамилия", name: "second_name", type: "text", validation: nameValidation}),
+            second_name: new FormInput({
+                label: "Фамилия",
+                name: "second_name",
+                type: "text",
+                validation: nameValidation
+            }),
             phone: new FormInput({label: "Телефон", name: "phone", type: "text", validation: phoneValidation}),
-            password: new FormInput({label: "Пароль", name: "password", type: "password", validation: passwordValidation}),
+            password: new FormInput({
+                label: "Пароль",
+                name: "password",
+                type: "password",
+                validation: passwordValidation
+            }),
         })
+    }
+
+    onSubmitHandler() {
+        getFormValues.apply(this)
+        onSubmitValidation(this.formValues, this.children)
     }
 
     protected render(): string {
