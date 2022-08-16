e3e24a7620eb0d65bb600ddd0523cc7162cef00d
semen.tishchenko <semen.tishchenko@glowbyteconsulting.com>
2022-08-17 00:23:12 +0300

    действия с чатом

--- src/components/chatActionMenu/chatActionMenu.ts
+++ src/components/chatActionMenu/chatActionMenu.ts
@@ -1,15 +1,36 @@
 import Block from '../../core/Block';
+import Button from '../button';
+import './chatActionMenu.less';
 
 export class ChatActionMenu extends Block {
 	constructor() {
 		super();
+		const deleteCurrentChat = new Button({
+			text: 'Удалить чат',
+			onSubmit: () => console.log('удалить пользователь чат')
+		});
+		const deleteUsersFromChat = new Button({
+			text: 'Удалить пользователей из чата', onSubmit: () => {
+				console.log('удалить всех пользователей из чата ');
+			}
+		});
+		this.setChildren({
+			deleteCurrentChat, deleteUsersFromChat
+		});
 	}
 
 	protected render(): string {
 		//language=hbs
 		return `
             <div class="chat-actions">
-                <p>мы сможем все</p>
+                <div class="chat-action">
+                    {{{deleteCurrentChat}}}
+                </div>
+                <div class="chat-action">
+                    {{{deleteUsersFromChat}}}
+                </div>
+               
+
             </div>
 		`;
 	}
