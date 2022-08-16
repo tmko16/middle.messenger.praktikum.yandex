40df15ca0bb29567cd41659120190470db80133b
semen.tishchenko <semen.tishchenko@glowbyteconsulting.com>
2022-08-17 00:23:12 +0300

    действия с чатом

--- src/components/chatActionMenu/chatActionMenu.ts
+++ src/components/chatActionMenu/chatActionMenu.ts
@@ -1,15 +1,46 @@
 import Block from '../../core/Block';
+import Button from '../button';
+import './chatActionMenu.less';
+import Store, {StoreEvents} from '../../core/Store';
 
 export class ChatActionMenu extends Block {
+	private store: Store;
 	constructor() {
 		super();
+		this.store = new Store();
+		const deleteCurrentChat = new Button({
+			text: 'Удалить чат',
+			onSubmit: () => console.log('удалить пользователь чат')
+		});
+		const deleteUsersFromChat = new Button({
+			text: 'Удалить пользователей из чата', onSubmit: () => {
+				console.log('удалить всех пользователей из чата ');
+			}
+		});
+		this.setChildren({
+			deleteCurrentChat, deleteUsersFromChat
+		});
+		this.store.on(StoreEvents.Updated, () => {
+			console.log(this.store);
+		});
+	}
+
+	componentDidMount(props: any) {
+		console.log(this.store.getState(), 'внутри модалки');
 	}
 
 	protected render(): string {
 		//language=hbs
 		return `
             <div class="chat-actions">
-                <p>мы сможем все</p>
+                <div class="chat-action">
+                    {{{deleteCurrentChat}}}
+                </div>
+                <div class="chat-action">
+                    {{{deleteUsersFromChat}}}
+                </div>
+               
+
             </div>
 		`;
 	}
