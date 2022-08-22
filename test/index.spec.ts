b13d4bd4aa45a91f2f195288f591a83cc7a736ad
tmko16 <simontis1991@gmail.com>
2022-08-22 22:49:09 +0300

    Добавлены тесты и конфиги к ним

---
+++ test/index.spec.ts
@@ -0,0 +1,12 @@
+import {expect} from 'chai';
+
+
+function hello(name: string) {
+	return `Hello ${name}`;
+}
+
+describe('Typescript + Babel usage suite', () => {
+	it('should return string correctly', () => {
+		expect(hello('mocha'), 'Hello mocha');
+	});
+});
