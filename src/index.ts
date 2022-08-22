20e132e5e7f8d548ee20104cce105e6b12f11241
tmko16 <simontis1991@gmail.com>
2022-08-22 20:49:45 +0300

    Добавлен husky

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,9 @@ import LoginPage from './pages/login';
 import RegistrationPage from './pages/registration';
 import ChatPage from './pages/chat';
 import ChangePassword from './pages/changePassword';
-import Page404 from './pages/404';
-import Page500 from './pages/500';
-import renderDOM from './core/renderDOM';
 import {Router} from './core/Router';
-import Button from './components/button';
 import Block from './core/Block';
-import IndexPage from './pages/indexPage';
-import HTTPTransport from './core/HTTPTransport';
 import Store from './core/Store';
-import DialogList from './components/dialogList';
-import {Authorised} from './types';
 import {ChangeAvatar} from './pages/changeAvatar';
 import ProfilePage from './pages/profilepage';
 import ProfilePageEdit from './pages/profileedit';
@@ -39,11 +31,10 @@ import ProfilePageEdit from './pages/profileedit';
  *
  * Регистрация пользоватля также работает.
  */
-console.log('индекс тс')
+console.log('индекс тс');
 document.addEventListener('DOMContentLoaded',
 	() => {
 		const router = new Router();
-		const store = new Store();
 		router.use('/', LoginPage as unknown as Block);
 		router.use('/registration', RegistrationPage as unknown as Block);
 		router.use('/profilePage', ProfilePage as unknown as Block);
