3fd692ba436b39f2ec73661f182074fd70eab3be
tmko16 <simontis1991@gmail.com>
2022-08-24 23:40:22 +0300

    обновил тесты

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,10 @@
-module.exports = api => {
+module.exports = (api) => {
 	// Cache configuration is a required option
 	api.cache(false);
 
 	const presets = [
 		'@babel/preset-typescript',
-		'@babel/preset-env'
+		'@babel/preset-env',
 	];
 
 	return { presets };
