94a2e9e84bf9238f39000a86e6b36ce0defec8cc
semen.tishchenko <semen.tishchenko@glowbyteconsulting.com>
2022-08-26 01:35:47 +0300

    feat: обновил readme и докерфайл

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const helmet = require("helmet");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(helmet.referrerPolicy({ policy: "same-origin" }));
 app.use(
